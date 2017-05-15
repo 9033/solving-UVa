9bf2f392d5bcbd480799944435b3f468361a6f6b
Lee JK <28668509+9033@users.noreply.github.com>
2017-05-15 21:03:39 +0900

    Update 10523 - Very Easy !!!.py

--- 10523 - Very Easy !!!.py
+++ 10523 - Very Easy !!!.py
@@ -3,6 +3,8 @@ import sys
 for line in sys.stdin:
     N, A = map(int,line.split()) #get two integers in a line
     S = 0;
+    Apowi = 1
     for i in range(1,N+1):
-        S = S + i * (A ** i)
+        Apowi = Apowi * A
+        S = S + i * (Apowi)
     print(S)
